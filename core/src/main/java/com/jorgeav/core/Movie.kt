634a117cdd2be2040bef72f4dc02be32f069ff93
jorgeavilae <jorgeavilae@gmail.com>
2020-06-08 14:13:32 +0200

    Add core module and update gradle files.

--- core/src/main/java/com/jorgeav/core/Movie.kt
+++ core/src/main/java/com/jorgeav/core/Movie.kt
@@ -13,7 +13,8 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
-include ':core'
 
-include ':app'
-rootProject.name = "LasPeliculas"
+package com.jorgeav.core
+
+class Movie {
+}
