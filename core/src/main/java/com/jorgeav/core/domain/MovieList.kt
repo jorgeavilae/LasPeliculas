7ab1efd959cac63099bfe5612d6aa3a9f7156bd8
jorgeavilae <jorgeavilae@gmail.com>
2020-06-08 15:38:34 +0200

    Add core domain data classes

---
+++ core/src/main/java/com/jorgeav/core/domain/MovieList.kt
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2020 Jorge Ávila
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.jorgeav.core.domain
+
+/**
+ * Created by Jorge Avila on 08/06/2020.
+ */
+data class MovieList(
+    val id: Int,
+    val name: String,
+    val description: String,
+    val results: List<MovieListItem>,
+    val creatorUsername: String
+)
+
+/*
+{
+  "iso_639_1": "es",
+--------------------------------------------------------------"id": 105937,
+  "page": 1,
+  "iso_3166_1": "ES",
+  "total_results": 2,
+  "object_ids": {
+    "movie:496243": "5a4db2bd0e0a26481d0042b8",
+    "movie:530915": "5b280c5ec3a36864c600166c"
+  },
+  "revenue": 613951714,
+  "total_pages": 1,
+----------------------------------------------------------------"name": "HDD HDD HDD",
+  "public": true,
+  "comments": {
+    "movie:496243": null,
+    "movie:530915": null
+  },
+  "sort_by": "original_order.asc",
+------------------------------------------------------------------"description": "Peliculas del HDD",
+  "backdrop_path": null,
+--------------------------------------------------------------------"results": [...]
+  "average_rating": 8.24,
+  "runtime": 252,
+  "created_by": {
+    "gravatar_hash": "c8b59687a5a791aa08edf664941bc8d6",
+    "name": "",
+----------------------------------------------------------------------"username": "jorgeavila",
+    "id": "58a81ad6c3a3686641005807"
+  },
+  "poster_path": null
+ */
---
+++ core/src/main/java/com/jorgeav/core/domain/MovieListItem.kt
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2020 Jorge Ávila
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.jorgeav.core.domain
+
+data class MovieListItem(
+    val id: Int,
+    val title: String,
+    val overview: String,
+    val posterUrl: String
+)
+
+/*
+{
+---------------------------------------------------------------"poster_path": "/axdhbcZeOsfX3ZpwtgdgPIjc06l.jpg",
+      "popularity": 98.403,
+      "vote_count": 5277,
+      "video": false,
+      "media_type": "movie",
+----------------------------------------------------------------"id": 530915,
+      "adult": false,
+      "backdrop_path": "/cqa3sa4c4jevgnEJwq3CMF8UfTG.jpg",
+      "original_language": "en",
+      "original_title": "1917",
+      "genre_ids": [
+        28,
+        18,
+        36,
+        10752
+      ],
+---------------------------------------------------------------"title": "1917",
+      "vote_average": 7.9,
+--------------------------------------------------------------"overview": "Nos encontramos en el año 1917. La Guerra Mundial amenazaba con cambiar, para siempre, el orden mundial. Ante la amenaza que se cernía, Estados Unidos decidió entrar en el conflicto con el objetivo de desequilibrar la balanza que caracterizaba a la contienda.",
+      "release_date": "2019-12-25"
+    }
+ */
