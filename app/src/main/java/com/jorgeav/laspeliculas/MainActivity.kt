96b5ce7220813983a074d5c5e1d8aabf7109fa2c
jorgeavilae <jorgeavilae@gmail.com>
2020-07-10 13:02:12 +0200

    Add Navigation Component with MainFragment and InsertListFragment

--- app/src/main/java/com/jorgeav/laspeliculas/MainActivity.kt
+++ app/src/main/java/com/jorgeav/laspeliculas/MainActivity.kt
@@ -35,11 +35,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
-                    .commitNow()
-        }
 
         WorkManager.getInstance(applicationContext).getWorkInfosForUniqueWorkLiveData("RefreshListCoroutineWorker")
             .observe(this, Observer { workInfos ->
